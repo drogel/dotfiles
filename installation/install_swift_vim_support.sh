e853f60247fd09efcfe6280d28a06e801a8d002c
Diego Rogel <50502687+drogel@users.noreply.github.com>
2021-05-27 23:19:34 +0200

    feat(swift-format): adds vim-autoformat support for swift (#44)

--- installation/install_swift_vim_support.sh
+++ installation/install_swift_vim_support.sh
@@ -32,6 +32,33 @@ if ! which jq > /dev/null; then
 	brew install jq
 fi
 
+# Install swift-format if it's not installed already, we need it for
+# autoformatting in vim.
+if ! which swift-format > /dev/null; then
+	echo "swift-format binary not found. I need it for autoformatting. Installing using homebrew..."
+	brew install swift-format
+fi
+
+# Since we cannot use dashes when declaring global variables in the .vimrc
+# file, and we need to declare a global variable named after the formatting
+# binary for vim-autoformat to use it, we will copy the swift-format binary to
+# apple_swiftformat. That way we can use swift-format with the vim-autoformat
+# plugin.
+appleSwiftFormat=apple_swiftformat
+if ! which appleSwiftFormat > /dev/null; then
+	echo "Copying swift-format to a binary called apple_swiftformat so that we can use it with vim-autoformat..."
+	appleSwiftFormatPath=$(which swift-format)
+	appleSwiftFormatParentPath=$(dirname $appleSwiftFormatPath)
+	cp $appleSwiftFormatPath $appleSwiftFormatParentPath/$appleSwiftFormat
+fi
+
+# Install swiftformat if it's not installed already, we need it for
+# autoformatting in vim.
+if ! which swiftformat > /dev/null; then
+	echo "swiftformat binary not found. I need it for autoformatting. Installing using homebrew..."
+	brew install swiftformat
+fi
+
 appendLineToFileIfNotPresent() {
 	lineToInsert=$1
 	file=$2
@@ -54,10 +81,16 @@ if [ ! -f "$vimrc" ]; then
 	touch $vimrc
 fi
 
-# Add the CoC plugin dependency into the .vimrc file if it's not already there.
+# Add the lines that define the vim-plug plugin declaration block if they are
+# not already there.
 appendLineToFileIfNotPresent "call plug#begin('~/.vim/plugged')" $vimrc
 appendLineToFileIfNotPresent "call plug#end()" $vimrc
+
+# Add the CoC plugin dependency into the .vimrc file if it's not already there.
 insertBeforeLineIfNotPresent "Plug 'neoclide/coc.nvim', {'branch': 'release'}" "call plug#end()" $vimrc
+
+# Add the vim-autoformat plugin dependency into the .vimrc file if it's not already there.
+insertBeforeLineIfNotPresent "Plug 'Chiel92/vim-autoformat'" "call plug#end()" $vimrc
 
 # Install the plugin we just appended to .vimrc.
 echo "Installing the plug.vim plugin manager plugins..."
@@ -66,6 +99,13 @@ vim +'PlugInstall --sync' +qall &> /dev/null
 # Install coc-sourcekit.
 echo "Installing coc-sourcekit..."
 vim -c 'CocInstall -sync coc-sourcekit|qall!' &> /dev/null
+
+# Tell the vim-autoplugin how to use the swiftformat and the apple_swiftformat
+# binaries.
+echo "Configuring the vim-autoformat plugin..."
+appendLineToFileIfNotPresent "let g:formatdef_swiftformat = '\"cat - | swiftformat --quiet stdin\"'" $vimrc
+appendLineToFileIfNotPresent "let g:formatdef_apple_swiftformat = '\"apple_swiftformat -\"'" $vimrc
+appendLineToFileIfNotPresent "let g:formatters_swift = ['apple_swiftformat', 'swiftformat']" $vimrc
 
 # Define the path where the CoC settings file is.
 cocSettingsFile=~/.vim/coc-settings.json
